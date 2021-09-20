1dc4ce421fb631bf75c75f1f90d623cb17b81068
kanojiaD <kanojia.uiet@gmail.com>
2021-09-20 10:21:23 +0530

    MissingNumber

---
+++ CSES/MissingNumber.java
@@ -0,0 +1,36 @@
+/*
+You are given all numbers between 1,2,…,n except one. Your task is to find the missing number.
+
+Input :The first input line contains an integer n.
+
+The second line contains n−1 numbers. Each number is distinct and between 1 and n (inclusive).
+
+Output: Print the missing number.
+
+Constraints:
+2≤n≤2⋅105
+
+Example
+
+Input:
+5
+2 3 1 5
+
+Output:
+4
+*/
+import java.util.*;
+class MissingNumber{
+	public static void main(String[] args) {
+		Scanner scan= new Scanner(System.in);
+		int n= scan.nextInt();
+		int cnt=n;
+		long sum=0l;
+		while(cnt>1)
+		{
+			sum+=scan.nextInt();
+			cnt--;
+		}
+		System.out.println((long)n*(n+1)/2-sum);
+	}
+}
